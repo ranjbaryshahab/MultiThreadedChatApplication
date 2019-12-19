1dd2c07987dd1bec16931df7ff4e836a9cdc0dde
shahab-7596 <shahab.ranjbary.1996@gmail.com>
2019-12-19 09:05:11 +0330

    UserList move to server class.

--- src/ir/maktab/java32/projects/multithreadedchatapplication/Server.java
+++ src/ir/maktab/java32/projects/multithreadedchatapplication/Server.java
@@ -3,12 +3,17 @@ package ir.maktab.java32.projects.multithreadedchatapplication;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class Server {
+    static ArrayList<User> userList = new ArrayList<>();
+    private final static String ipAddress = "127.0.0.1";
+    private final static int portAddress = 3000;
+
     public static void main(String[] args) throws IOException {
-        ServerSocket server = new ServerSocket(3000);
+        ServerSocket server = new ServerSocket(portAddress);
         System.out.println("Server is ready for chat");
         ExecutorService executorService = Executors.newFixedThreadPool(50);
         while (true) {
